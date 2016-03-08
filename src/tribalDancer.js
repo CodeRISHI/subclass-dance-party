a1561bd413cb10752e98a9fb3dd7f5cbe8d0539e
CodeRISHI <arunv83@gmail.com>
2016-03-07 20:51:13 -0800

    Update GIFs to work on a single line

--- src/tribalDancer.js
+++ src/tribalDancer.js
@@ -2,6 +2,8 @@ var TribalDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
 
   this.$node.addClass('tribal');
+
+  // this.$node = $('<img src="images/drake-gif-5.gif" style="width:200px;height:200px;">');
 };
 
 TribalDancer.prototype = Object.create(Dancer.prototype);
@@ -10,6 +12,6 @@ TribalDancer.prototype.constructor = TribalDancer;
 TribalDancer.prototype.oldStep = Dancer.prototype.step;
 
 TribalDancer.prototype.step = function() {
-  this.oldStep();
+  // this.oldStep();
   this.$node.toggle();
 };
